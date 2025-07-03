d35a59c5dade4310b10ba715ee7f3627b49d9dee
root <root@deletepc.localdomain>
2025-07-04 01:24:05 +0200

    [Dorado-AI-DevOps] Uploaded Changes

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 
 # Configuraciones de redirección
 SERVICES = {
-    "lint_chart": os.getenv("SERVICE_LINT_CHART", "http://helm-linter-service.devops-ai.svc.cluster.local:80/lint_chart"),
+    "lint_chart": os.getenv("SERVICE_LINT_CHART", "http://helm-linter-service.devops-ai.svc.cluster.local:80/lint-chart"),
     "analyze_log": os.getenv("SERVICE_ANALYZE_LOG", "http://log-analyzer-service.devops-ai.svc.cluster.local:80/analyze"),
     "generate_pipeline": os.getenv("SERVICE_GENERATE_PIPELINE", "http://pipeline-gen-service.devops-ai.svc.cluster.local:80/generate")
 }
